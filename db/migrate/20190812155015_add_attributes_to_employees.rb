fc9fbe7cd330f9342110237fbb8bb2d7ae2dadf0
roshita02 <roshitashakya07@gmail.com>
2019-08-12 21:36:33 +0545

    Attributes added to Employee

---
+++ db/migrate/20190812155015_add_attributes_to_employees.rb
@@ -0,0 +1,6 @@
+class AddAttributesToEmployees < ActiveRecord::Migration[6.0]
+  def change
+    add_column :employees, :first_name, :string
+    add_column :employees, :last_name, :string
+  end
+end
