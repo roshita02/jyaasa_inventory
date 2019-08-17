1ff7ea08cd6e2b153482d4607705744a286d3c5e
roshita02 <roshitashakya07@gmail.com>
2019-08-17 23:30:52 +0545

    Issues in item fixed

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,6 @@
 
 # Model for base class item
 class Item < ApplicationRecord
-<<<<<<< HEAD
-  validates_presence_of :name, :rate
-  belongs_to :vendor
-=======
   validates_presence_of :name, :quantity
   belongs_to :vendor, class_name: "Vendor", foreign_key: "vendor_id", optional: true
->>>>>>> feature/item
 end
