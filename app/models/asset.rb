f553d09d05aab3afa129bcdd487a326eefe3cf56
roshita02 <roshitashakya07@gmail.com>
2019-09-09 02:57:24 +0545

    assets removed

--- app/models/asset.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Model for asset
-class Asset < ApplicationRecord
-  validates_presence_of :name
-  belongs_to :category
-  validates :name, uniqueness: { case_sensitive: false }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,125 +10,129 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_908_140_200) do
+ActiveRecord::Schema.define(version: 2019_09_08_210820) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_admin_comments', force: :cascade do |t|
-    t.string 'namespace'
-    t.text 'body'
-    t.string 'resource_type'
-    t.bigint 'resource_id'
-    t.string 'author_type'
-    t.bigint 'author_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[author_type author_id], name: 'index_active_admin_comments_on_author_type_and_author_id'
-    t.index ['namespace'], name: 'index_active_admin_comments_on_namespace'
-    t.index %w[resource_type resource_id], name: 'index_active_admin_comments_on_resource_type_and_resource_id'
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.string "author_type"
+    t.bigint "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  create_table 'admin_users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_admin_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_admin_users_on_reset_password_token', unique: true
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  create_table 'assets', force: :cascade do |t|
-    t.string 'name'
-    t.string 'type'
-    t.integer 'quantity'
-    t.bigint 'category_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['category_id'], name: 'index_assets_on_category_id'
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.float "depreciation_rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'name'
-    t.string 'type'
-    t.float 'depreciation_rate'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "employees", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
+    t.index ["email"], name: "index_employees_on_email", unique: true
+    t.index ["invitation_token"], name: "index_employees_on_invitation_token", unique: true
+    t.index ["invitations_count"], name: "index_employees_on_invitations_count"
+    t.index ["invited_by_id"], name: "index_employees_on_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_employees_on_invited_by_type_and_invited_by_id"
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
-  create_table 'employees', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'invitation_token'
-    t.datetime 'invitation_created_at'
-    t.datetime 'invitation_sent_at'
-    t.datetime 'invitation_accepted_at'
-    t.integer 'invitation_limit'
-    t.string 'invited_by_type'
-    t.bigint 'invited_by_id'
-    t.integer 'invitations_count', default: 0
-    t.index ['email'], name: 'index_employees_on_email', unique: true
-    t.index ['invitation_token'], name: 'index_employees_on_invitation_token', unique: true
-    t.index ['invitations_count'], name: 'index_employees_on_invitations_count'
-    t.index ['invited_by_id'], name: 'index_employees_on_invited_by_id'
-    t.index %w[invited_by_type invited_by_id], name: 'index_employees_on_invited_by_type_and_invited_by_id'
-    t.index ['reset_password_token'], name: 'index_employees_on_reset_password_token', unique: true
+  create_table "item_assignments", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "employee_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["employee_id"], name: "index_item_assignments_on_employee_id"
+    t.index ["item_id"], name: "index_item_assignments_on_item_id"
   end
 
-  create_table 'item_assignments', force: :cascade do |t|
-    t.integer 'quantity'
-    t.bigint 'employee_id', null: false
-    t.bigint 'item_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['employee_id'], name: 'index_item_assignments_on_employee_id'
-    t.index ['item_id'], name: 'index_item_assignments_on_item_id'
+  create_table "item_requests", force: :cascade do |t|
+    t.string "item"
+    t.integer "quantity"
+    t.text "reason"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "employee_id"
+    t.integer "status", default: 2
   end
 
-  create_table 'item_requests', force: :cascade do |t|
-    t.string 'item'
-    t.integer 'quantity'
-    t.text 'reason'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'employee_id'
-    t.integer 'status', default: 2
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.integer "rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "vendor_id"
+    t.integer "quantity"
+    t.bigint "category_id"
+    t.date "purchased_date"
+    t.integer "assigned_quantity"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["vendor_id"], name: "index_items_on_vendor_id"
   end
 
-  create_table 'items', force: :cascade do |t|
-    t.string 'name'
-    t.string 'type'
-    t.integer 'rate'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'vendor_id'
-    t.integer 'quantity'
-    t.bigint 'category_id'
-    t.date 'purchased_date'
-    t.integer 'assigned_quantity'
-    t.index ['category_id'], name: 'index_items_on_category_id'
-    t.index ['vendor_id'], name: 'index_items_on_vendor_id'
+  create_table "purchases", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "rate"
+    t.date "purchased_date"
+    t.bigint "item_id", null: false
+    t.bigint "vendor_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_purchases_on_item_id"
+    t.index ["vendor_id"], name: "index_purchases_on_vendor_id"
   end
 
-  create_table 'vendors', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'pan_no'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "vendors", force: :cascade do |t|
+    t.string "name"
+    t.integer "pan_no"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'assets', 'categories'
-  add_foreign_key 'item_assignments', 'employees'
-  add_foreign_key 'item_assignments', 'items'
-  add_foreign_key 'items', 'categories'
-  add_foreign_key 'items', 'vendors'
+  add_foreign_key "item_assignments", "employees"
+  add_foreign_key "item_assignments", "items"
+  add_foreign_key "items", "categories"
+  add_foreign_key "items", "vendors"
+  add_foreign_key "purchases", "items"
+  add_foreign_key "purchases", "vendors"
 end
